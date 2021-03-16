6368b45310cf2e371d4ec18b7a0fe611f13fff4c
b3lzebuth <leandrofelipecunha@hotmail.com>
2021-03-15 22:38:28 -0300

    melhorias responsividade e adição de novas páginas

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ const toggleMenu = () => {
 
   let y = document.getElementById("cont-menu");
   if (y.className === "cont-menu") {
-    y.className = "cont-menu-open";
+    y.className = "cont-menu open";
     y.style.opacity = 0;
     setTimeout(() => {
       y.style.opacity = 1;
@@ -16,5 +16,27 @@ const toggleMenu = () => {
   } else {
     y.style.opacity = 1;
     y.className = "cont-menu";
+  }
+};
+
+const validaForm = () => {
+  inpName = document.getElementById("inp-name").value;
+  inpEmail = document.getElementById("inp-email").value;
+  inpSubject = document.getElementById("inp-subject").value;
+  inpMsg = document.getElementById("inp-msg").value;
+
+  inpAlert = document.getElementById("inp-alert");
+
+  if(inpName || inpEmail || inpSubject || inpMsg){
+    return true
+  } else {
+    inpAlert.className = "alert alert-danger";
+  }
+  
+};
+
+const enviarMensagem = () => {
+  if(validaForm()){
+    document.getElementById("form").submit();
   }
 };
